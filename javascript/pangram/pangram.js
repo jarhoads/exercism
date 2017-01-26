42c63f6899c0c758d775a133aaa85bbf3616f8c2
Josh <jarhoads@gmail.com>
2017-01-26 12:46:47 -0500

    making unique() easier to read

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -2,7 +2,9 @@ var Pangram = function(phrase){
     var lower_letters = "abcdefghijklmnopqrstuvwxyz";
 
     var isLetter = function(c){ return lower_letters.indexOf(c) >= 0; }
-    var unique = function(acc, curr, idx, arr){ return acc + ((arr.slice(idx+1).indexOf(curr) >= 0) ? "" : curr); };
+    var unique = function(acc, curr, idx, arr){
+        return acc + ((arr.slice(idx+1).indexOf(curr) >= 0) ? "" : curr);
+    };
 
     var letters = phrase.toLowerCase()
                         .split('')
